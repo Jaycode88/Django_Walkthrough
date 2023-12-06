f75498a52341ad2d37fb509ec787d574fd3501ba
Joe Seabrook <joeseabrook0306@gmail.com>
2023-12-06 18:33:19 +0000

    I cant believe it

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -182,7 +182,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 if 'USE_AWS' in os.environ:
     # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
-        'expires': 'Thu, 31 dec 2099 20:00:00 GMT',
+        'expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
 
