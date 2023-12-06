e8516493c66cf5bbf33a763dc279e10d2a69e39b
Joe Seabrook <joeseabrook0306@gmail.com>
2023-12-06 18:40:56 +0000

    edit object parameters

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -182,8 +182,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 if 'USE_AWS' in os.environ:
     # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
-        'expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-        'CacheControl': 'max-age=94608000',
+        'CacheControl': 'max-age=86400',
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
     }
 
     AWS_STORAGE_BUCKET_NAME = 'boutiqueadowalkthrough'
